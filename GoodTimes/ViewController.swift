73f3bc7eb4671680530b95cdb0f8a91ded7deb20
Ben Winters <ben@blwinters.com>
2020-02-28 13:51:58 -0500

    Initial Commit

---
+++ GoodTimes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodTimes
+//
+//  Created by Ben Winters on 2/28/20.
+//  Copyright © 2020 Ben Winters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
